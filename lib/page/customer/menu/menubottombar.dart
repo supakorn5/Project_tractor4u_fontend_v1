033e12fff4d597a2549d72e5c3f9a87bdd86c3b5
supakorn5 <64011212053@msu.ac.th>
2024-05-31 14:41:54 +0700

    workplace

---
+++ lib/page/customer/menu/menubottombar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenuBottombar extends StatefulWidget {
+  const MenuBottombar({super.key});
+
+  @override
+  State<MenuBottombar> createState() => _MenuBottombarState();
+}
+
+class _MenuBottombarState extends State<MenuBottombar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
